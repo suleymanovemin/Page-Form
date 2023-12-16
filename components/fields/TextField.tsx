56bbf725c86000cbe11c763b86e9a7bcad19eed5
suleymanovemin <suleymanov.emin2001@gmail.com>
2023-12-16 23:24:32 +0400

    ffff

--- components/fields/TextField.tsx
+++ components/fields/TextField.tsx
@@ -24,7 +24,7 @@ export const TextFieldFormElement: FormElement = {
   designerComponent: DesignerComponent,
   formComponent: () => <div>Form component</div>,
   propertiesComponent: () => <div>Properties component</div>,
-  // 
+  // s
 };
 
 function DesignerComponent({
